1861dbbbc8d16846f63a03a8e06584130efd01a0
Anpatil1 <nangareavadhut@gmail.com>
2024-08-05 16:04:56 +0530

    change  files

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://expense-mgmt-production.up.railway.app';
+const API_URL = 'https://expense-backend-ts5x.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
